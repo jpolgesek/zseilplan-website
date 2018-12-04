bb7415bab71ef37f80f32477fc5286a427aafe68
Jakub Półgęsek <jakub@polgesek.pl>
2018-12-04 20:55:36 +0100

    Add info about disabling theme

---
+++ assets/themes/christmas/christmas.js
@@ -0,0 +1,3 @@
+document.getElementById("remote_info").innerHTML = "Idą święta, pora na motyw świąteczny. Nie chcesz go widzieć? Wyłącz go <a href='#' onclick=\"preferences.set('disable_auto_themes', true, true); document.location.reload();\">na zawsze</a> lub <a href='#' onclick=\"preferences.set('disable_auto_themes_once', true, true); document.location.reload();\">tylko raz</a>";
+document.getElementsByClassName("tableTopInfo")[0].appendChild(document.getElementById("remote_info"));
+document.getElementById("remote_info").style.display = null;
