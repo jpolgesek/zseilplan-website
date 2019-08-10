4fdf23b6619adeffa9671e8a2b312182ff5ed26d
Jakub Półgęsek <jakub@polgesek.pl>
2019-08-11 00:30:47 +0200

    Add findFreeRooms demo

--- assets/js/app.tools.js
+++ assets/js/app.tools.js
@@ -11,6 +11,11 @@ app.tools = {
 		btn2 = app.utils.createEWC("button", ["content_btn"], '<i class="fas fa-history"></i> Wyświetl poprzednią wersję planu');
 		btn3 = app.utils.createEWC("button", ["content_btn"], '<i class="fas fa-dice"></i> Zmiany w planie');
 
+		btn1.onclick = function(){
+			toolselectDiv.close();
+			app.tools.findFreeRooms.modal();
+		}
+
 		toolselectDiv.sectionContent.appendChildren([
 			app.ui.modal.createRow(desc),
 			app.ui.modal.createRow(btn1),
@@ -21,10 +26,77 @@ app.tools = {
 		//FIXME: layout
 		toolselectDiv.sectionContent.style.padding = "10px";
 
-		row = app.ui.modal.createRow();
-		row.style.opacity = "0";
-		toolselectDiv.appendChild(row);
+		toolselectDiv.appendChild(app.ui.modal.createRow());
 		
 		toolselectDiv.show();
+	},
+
+	findFreeRooms: {
+		find: function(day, hour){
+			console.log("getFreeRooms", "Start: wolne sale");
+			var available_rooms = []
+			var dtt = data.timetable;
+			for (r in data.classrooms){
+				var classroom = data.classrooms[r];
+				try {
+					var found = false;
+					for (unit in dtt[day][hour]){
+						var itemsData = dtt[day][hour][unit];
+						itemsData = itemsData.filter(function(v){return v.s == classroom;});
+						if (itemsData.length != 0){
+							found = true;
+						}
+					}
+					if (!found){
+						available_rooms.push(classroom);
+					}
+				}catch (e){utils.err("utils.getFreeRooms", "Error: " + e);}
+			}
+			console.log("getFreeRooms", "Znaleziono " + available_rooms.length + " sal.");
+			return available_rooms;
+		},
+
+		modal: function(){
+			findFreeRoomsDiv = app.ui.modal.createTabbed({title: "Znajdź wolne sale", tabbed: false});
+
+			row = app.ui.modal.createRow();
+
+			tools_desc = `Tutaj znajdzie się jakiś ładny opis wyjaśniający jak to działa.`
+			
+			desc = app.utils.createEWC("p", ["desc"], tools_desc);
+			text_result = app.utils.createEWC("p", ["desc"], "");
+
+			input_day = app.utils.createEWC("input");
+			input_hour = app.utils.createEWC("input");
+			input_day.placeholder = "Dzień tygodnia";
+			input_hour.placeholder = "Godzina lekcyjna";
+			input_day.type = input_hour.type = "number";
+
+			button_search = app.utils.createEWC("button", ["content_btn"], '<i class="fas fa-search-location"></i> Szukaj');
+
+			button_search.onclick = function(){
+				r = app.tools.findFreeRooms.find(input_day.value, input_hour.value);
+				if (!r.length){
+					text_result.innerHTML = "Niestety, żadna sala lekcyjna nie jest wolna.";
+				}else{
+					text_result.innerHTML = `Następujące sale powinny być wolne: ${r.join(", ")}`;
+				}
+			}
+
+			findFreeRoomsDiv.sectionContent.appendChildren([
+				app.ui.modal.createRow(desc),
+				app.ui.modal.createRow(input_day),
+				app.ui.modal.createRow(input_hour),
+				app.ui.modal.createRow(text_result),
+				app.ui.modal.createRow(button_search)
+			]);
+
+			//FIXME: layout
+			findFreeRoomsDiv.sectionContent.style.padding = "10px";
+
+			findFreeRoomsDiv.appendChild(app.ui.modal.createRow());
+			
+			findFreeRoomsDiv.show();
+		}
 	}
 }
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -129,29 +129,6 @@ var utils = {
 			dom.addClass(document.getElementById("android_instruction"), "anim");
 		}, 1)
 	},
-	getFreeRooms: function(day, hour){
-		this.log("getFreeRooms", "Start: wolne sale");
-		var available_rooms = []
-		var dtt = data.timetable;
-		for (r in data.classrooms){
-			var classroom = data.classrooms[r];
-			try {
-				var found = false;
-				for (unit in dtt[day][hour]){
-					var itemsData = dtt[day][hour][unit];
-					itemsData = itemsData.filter(function(v){return v.s == classroom;});
-					if (itemsData.length != 0){
-						found = true;
-					}
-				}
-				if (!found){
-					available_rooms.push(classroom);
-				}
-			}catch (e){utils.err("utils.getFreeRooms", "Error: " + e);}
-		}
-		this.log("getFreeRooms", "Znaleziono " + available_rooms.length + " sal.");
-		return available_rooms;
-	},
 
 	getFreeRoomsUI: function(day, hour){
 		/*
