381827946b599fe66d9f8331cb70b854b348ccfd
Jakub Półgęsek <jakub@polgesek.pl>
2019-08-31 02:46:20 +0200

    Enable compattest

--- assets/js/compattest.js
+++ assets/js/compattest.js
@@ -1,3 +1,5 @@
+var is_compatible = true;
+
 try {
 	console.log();
 } catch (e) {
@@ -6,18 +8,20 @@ try {
 		error: alert,
 		warn: alert
 	}
+	is_compatible = false;
 }
-
-var is_compatible = true;
-
 if (!window.fetch){
 	console.log("No fetch in browser");
-	is_compatible = false;
+	// is_compatible = false;
 }
 
 if (!document.querySelector){
 	console.log("No querySelector in browser");
 	is_compatible = false;
+}
+
+if (!is_compatible){
+	document.location.href = "ie_index.html";
 }
 
 //ie
